2bfccf88fbe64859acd47591f0a3ce77f6e027fa
Yarosh Darya <59346874+darya-yarosh@users.noreply.github.com>
2026-04-25 19:57:43 +0300

    [A-2/feat]: Create method of clearing page (stage).

---
+++ src/utils/clearStage.js
@@ -0,0 +1,11 @@
+import { Application } from "pixi.js";
+
+/**
+ * Clearing child-elements from stage.
+ * Because all stages (pages) created using the wrapper element first, we can just remove first children.
+ * 
+ * @param {Application[stage]} stage - stage for removing child-elements.
+ */
+export const clearStage = (stage) => {
+    stage.removeChildren(0);
+}
