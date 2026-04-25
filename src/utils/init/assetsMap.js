a595180427d9b44c3e76007c6041033c6309af75
Yarosh Darya <59346874+darya-yarosh@users.noreply.github.com>
2026-04-25 19:48:24 +0300

    [A-2/feat]: Create constant with assets.

---
+++ src/utils/init/assetsMap.js
@@ -0,0 +1,11 @@
+const assetsMap = {
+    sprites: [
+        { alias: "BG", src: '../src/assets/interface/bg.avif' },
+        { alias: "ButtonDefault", src: '../src/assets/interface/ButtonDefault.png' },
+        { alias: "ButtonDown", src: '../src/assets/interface/ButtonDown.png' },
+        { alias: "ButtonOver", src: '../src/assets/interface/ButtonOver.png' },
+        { alias: "SoundActive", src: '../src/assets/interface/sound.png' },
+    ],
+}
+
+export default assetsMap;
