a27b0b14201e20c037beda9ecd89333170f14c2c
Yarosh Darya <59346874+darya-yarosh@users.noreply.github.com>
2026-04-26 03:49:18 +0300

    [A-3/feat]: Create MenuPage.

---
+++ src/pages/MenuPage.js
@@ -0,0 +1,101 @@
+import { Container, Text, Sprite } from "pixi.js";
+
+import { PAGES, STATE } from "../main";
+
+import Icon from "../models/Icon";
+import ButtonList from "../models/ButtonList";
+import { Logo } from "../ui/Interface/Logo";
+import { TextData } from "../constants/interface";
+
+import { clearStage } from "../utils/clearStage";
+import { setBackground } from "../utils/setBackground";
+
+const createHeader = () => {
+    const headerContainter = new Container();
+    headerContainter.width = STATE.app.screen.width;
+
+    const logo = Logo();
+    logo.anchor.set(0.5);
+    
+    headerContainter.height = logo.height;
+
+    logo.position.set(
+        STATE.app.screen.width * 0.5,
+        logo.height
+    );
+
+    headerContainter.addChild(logo);
+    return headerContainter;
+}
+
+const createNav = () => {
+    const navContainer = new Container();
+    navContainer.width = STATE.app.screen.width;
+
+    const buttonWidth = STATE.app.screen.width <= 440
+        ? STATE.app.screen.width - 32
+        : 440
+    const buttonHeight = 80;
+
+    navContainer.height = buttonHeight;
+
+    const buttonPositions = [
+        {
+            position: {
+                x: (STATE.app.screen.width - buttonWidth) * 0.5,
+                y: buttonHeight,
+            },
+            text: "Start",
+            action: () => {
+                // STATE.currentPage = PAGES.levelsList;
+                // STATE.currentPage.draw();
+            },
+            width: buttonWidth,
+            height: buttonHeight,
+        },
+    ];
+
+    const buttonsList = new ButtonList(buttonPositions);
+    buttonsList.view.forEach((button) => {
+        navContainer.addChild(button.view)
+    })
+
+    return navContainer;
+}
+
+const createSoundButton = () => {
+    const iconSize = 96;
+    const iconX = STATE.app.screen.width <= 500
+        ? 40
+        : 60;
+    const iconY = STATE.app.screen.height - iconX - iconSize;
+
+    return new Icon(iconX, iconY, undefined, iconSize, iconSize, () => {});;
+}
+
+/**
+ * 
+ * @param {Container} currentStage 
+ */
+const DrawMenu = (currentStage) => {
+    clearStage(currentStage);
+
+    const menuContainer = new Container();
+    setBackground(Sprite.from("BGMenu"), menuContainer);
+
+    const headerContainter = createHeader();
+    headerContainter.position.set(0,100)
+    menuContainer.addChild(headerContainter);
+
+    const nav = createNav();
+    nav.position.set(0,200)
+    menuContainer.addChild(nav);
+
+    const icon = createSoundButton();
+    menuContainer.addChild(icon.view);
+
+    
+    currentStage.addChild(menuContainer);
+}
+
+export default DrawMenu;
