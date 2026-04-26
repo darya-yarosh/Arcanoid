fdb820717eecbbfa9fd023a020d7ee51f5b35677
Yarosh Darya <59346874+darya-yarosh@users.noreply.github.com>
2026-04-26 03:44:52 +0300

    [A-3/feat]: Create Button model.

---
+++ src/models/Button.js
@@ -0,0 +1,97 @@
+import { Container, Sprite, Text, Texture } from "pixi.js";
+
+export const ButtonData = {
+    width: 226,
+    height: 96,
+    textureDefault: 'ButtonDefault',
+    textureDown: 'ButtonDown',
+    textureOver: 'ButtonOver',
+    textFontFamily: 'TunnelFront',
+    textColorDefault: 'white',
+    textColorDown: '#636363',
+    textColorOver: '#636363',
+}
+
+export default class Button {
+    constructor(positionX, positionY, text, action, width = ButtonData.width, height = ButtonData.height) {
+        this._view = new Container();
+        this._view.eventMode = 'passive';
+
+        this._button = new Sprite(Texture.from(ButtonData.textureDefault));
+        this._button.x = positionX;
+        this._button.y = positionY;
+        this._button.width = width;
+        this._button.height = height;
+        this._button.cursor = 'pointer';
+        this._button.eventMode = 'static';
+        this._button
+            .on('pointerdown', () => this.onButtonDown())
+            .on('pointerup', () => this.onButtonUp(action))
+            .on('pointerupoutside', () => this.onButtonUpOutside())
+            .on('pointerover', () => this.onButtonOver())
+            .on('pointerout', () => this.onButtonOut());
+        this._button.isDown = false;
+        this._button.isOver = false;
+
+        this._text = new Text(text, {
+            fontFamily: ButtonData.textFontFamily,
+            fontSize: 32,
+            fill: ButtonData.textColorDefault,
+            align: 'center',
+        });
+        this._text.eventMode = 'none';
+        this._text.x = positionX + (width - this._text.width) / 2;
+        this._text.y = positionY + (height - this._text.height) / 2;
+
+        this._view.addChild(this._button, this._text);
+    }
+
+    get view() {
+        return this._view;
+    }
+
+    onButtonDown() {
+        this._button.texture = Texture.from(ButtonData.textureDown);
+        this._button.isDown = true;
+
+        this._text.style.fill = ButtonData.textColorDown;
+    }
+
+    onButtonUp(action) {
+        console.log("action")
+        action();
+
+        this._button.texture = Texture.from(ButtonData.textureDefault);
+        this._button.isDown = false;
+
+        this._text.style.fill = ButtonData.textColorDefault;
+    }
+
+    onButtonUpOutside() {
+        this._button.isDown = false;
+        this._button.texture = Texture.from(ButtonData.textureDefault);
+
+        this._text.style.fill = ButtonData.textColorDefault;
+    }
+
+    onButtonOver() {
+        this._button.isOver = true;
+        if (this._button.isDown) {
+            return;
+        }
+
+        this._button.texture = Texture.from(ButtonData.textureOver);
+        this._text.style.fill = ButtonData.textColorOver;
+    }
+
+    onButtonOut() {
+        this._button.isOver = false;
+        if (this._button.isDown) {
+            return;
+        }
+
+        this._button.texture = Texture.from(ButtonData.textureDefault);
+
+        this._text.style.fill = ButtonData.textColorDefault
+    }
+}
