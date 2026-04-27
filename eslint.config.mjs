340b8a96219dd60e26a7b0b2240af738b1c45eb7
Yarosh Darya <59346874+darya-yarosh@users.noreply.github.com>
2026-04-27 03:28:16 +0300

    [A-6/feat]: Set new configurations for eslint.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,25 @@
+
+import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import prettier from "eslint-plugin-prettier/recommended";
+import globals from "globals";
 
-export default [
+export default defineConfig([
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, prettier],
+    extends: [
+      js.configs.recommended,
+    ],
     files: ["**/*.{js,jsx}"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
+      globals: {
+        ...globals.browser,
+      },
     },
-    rules: {},
+    rules: {
+      semi: ["warn", "always"],
+    },
   },
-];
+]);
